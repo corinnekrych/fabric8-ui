28d490626e20b660fead7b3307b19f3cee457d94
Corinne Krych <corinnekrych@gmail.com>
2017-05-24 11:05:02 +0200

    remove unsused test

--- src/app/create/codebases/codebases-add/codebases-add.component.spec.ts
+++ src/app/create/codebases/codebases-add/codebases-add.component.spec.ts
@@ -88,7 +88,7 @@ describe('Codebases Add Component', () => {
     fixture = TestBed.createComponent(CodebasesAddComponent);
   });
 
-  xit('Init component succesfully', async(() => {
+  it('Init component succesfully', async(() => {
     // given
     const comp = fixture.componentInstance;
     const debug = fixture.debugElement;
@@ -102,7 +102,7 @@ describe('Codebases Add Component', () => {
     });
   }));
 
-xit('Display github repo details after sync button pressed', async(() => {
+it('Display github repo details after sync button pressed', async(() => {
     // given
     gitHubServiceMock.getRepoDetailsByFullName.and.returnValue(Observable.of(expectedGitHubRepoDetails));
     gitHubServiceMock.getRepoLicenseByUrl.and.returnValue(Observable.of(expectedGitHubRepoLicense));
@@ -150,7 +150,7 @@ xit('Display github repo details after sync button pressed', async(() => {
     });
   }));
 
-  xit('Add codebase to space', async(() => {
+  it('Add codebase to space', async(() => {
     // given
     gitHubServiceMock.getRepoDetailsByFullName.and.returnValue(Observable.of(expectedGitHubRepoDetails));
     gitHubServiceMock.getRepoLicenseByUrl.and.returnValue(Observable.of(expectedGitHubRepoLicense));
